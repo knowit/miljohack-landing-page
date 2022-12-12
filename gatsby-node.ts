73728d87a03dd85368a2d4219f2c6f65a0780cfa
Christopher Gjøvåg <christopher.gjovag@knowit.no>
2022-12-12 14:26:51 +0100

    Add typing to gatsby-node

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,12 +1,13 @@
+import { CreateSchemaCustomizationArgs } from 'gatsby';
 
-export const createSchemaCustomization = ({actions}) => {
-  const { createTypes } = actions
-  
+export const createSchemaCustomization = ({ actions }: CreateSchemaCustomizationArgs) => {
+  const { createTypes } = actions;
+
   // Explicitly define path such that a single null can exist
   const FrontpageCardsLinkType = `
     type FrontpageCardsLink implements Node {
       path: String
     }
-  `
-  createTypes([FrontpageCardsLinkType])
-}
+  `;
+  createTypes([FrontpageCardsLinkType]);
+};
